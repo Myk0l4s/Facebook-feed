ea27fac900d0fc97f905e595b61f835e11082cd7
Myk0l4s <morkunas.mykolas@gmail.com>
2021-05-07 20:08:53 +0300

    javascript beg

---
+++ js/data.js
@@ -0,0 +1,216 @@
+"use strict";
+
+const feed = [{
+        autorius: {
+            vardas: 'Tomas',
+            pavarde: 'Pavardenis',
+            amzius: 0,
+            lytis: 'Vyras',
+            avataras: '',
+            nuoroda: '#'
+        },
+        laikas: 1583832433393,
+        pranesimas: {
+            tekstas: 'Sveiki, as esu Tomas.',
+            spalva: '',
+            paveiksliukai: [
+
+            ]
+        }
+    },
+    {
+        autorius: {
+            vardas: 'Laura',
+            pavarde: 'Pavardenis',
+            amzius: 20,
+            lytis: 'Moteris',
+            avataras: '',
+            nuoroda: '#',
+        },
+        laikas: 1583832433393,
+        pranesimas: {
+            tekstas: 'Neturiu komentaru....',
+            spalva: '',
+            paveiksliukai: [
+
+            ]
+        }
+    },
+    {
+        autorius: {
+            vardas: 'Tomas',
+            pavarde: 'Pavardenis',
+            amzius: 27,
+            lytis: 'Vyras',
+            avataras: '2.png',
+            nuoroda: '#',
+        },
+        laikas: 1583832434388,
+        pranesimas: {
+            tekstas: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex earum tenetur odit nemo id architecto laborum iure error, aliquid nisi',
+            spalva: '',
+            paveiksliukai: [
+
+            ]
+        }
+    },
+    {
+        autorius: {
+            vardas: 'Vardenis',
+            pavarde: 'Pavardenis',
+            amzius: 50,
+            lytis: 'Vyras',
+            avataras: '3.png',
+            nuoroda: '#',
+        },
+        laikas: 1583832434388,
+        pranesimas: {
+            tekstas: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex earum tenetur odit nemo id architecto laborum iure error, aliquid nisi . Ex earum tenetur odit nemo id architecto laborum iure error, . Ex earum tenetur odit nemo id architecto laborum iure error,',
+            spalva: '',
+            paveiksliukai: [
+
+            ]
+        }
+    },
+    {
+        autorius: {
+            vardas: 'Vardenis',
+            pavarde: 'Pavardenis',
+            amzius: 50,
+            lytis: 'Vyras',
+            avataras: 'avataras.png',
+            nuoroda: '#',
+        },
+        laikas: 1583832434388,
+        pranesimas: {
+            tekstas: 'Labas',
+            spalva: '',
+            paveiksliukai: [
+
+            ]
+        }
+    },
+    {
+        autorius: {
+            vardas: 'Vardenis',
+            pavarde: 'Pavardenis',
+            amzius: 50,
+            lytis: 'Vyras',
+            avataras: 'avataras.png',
+            nuoroda: '#',
+        },
+        laikas: 1583832434388,
+        pranesimas: {
+            tekstas: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, veniam quia totam nemo, tempore delectus, illum rem necessitatibus minus maxime et odit vel explicabo ullam! Tenetur iusto velit delectus et ex numquam repellat natus, eaque esse sint iste consectetur placeat nesciunt quos vel commodi? Aliquam modi facilis nulla consequatur illum.',
+            spalva: '',
+            paveiksliukai: [
+
+            ]
+        }
+    },
+    {
+        autorius: {
+            vardas: 'Vardenis',
+            pavarde: 'Pavardenis',
+            amzius: 50,
+            lytis: 'Vyras',
+            avataras: 'avataras.png',
+            nuoroda: '#',
+        },
+        laikas: 1583832434388,
+        pranesimas: {
+            tekstas: 'Labas',
+            spalva: 'red',
+            paveiksliukai: [
+                '1.png',
+                '2.png',
+            ]
+        }
+    },
+    {
+        autorius: {
+            vardas: 'Vardenis',
+            pavarde: 'Pavardenis',
+            amzius: 50,
+            lytis: 'Vyras',
+            avataras: 'avataras.png',
+            nuoroda: '#',
+        },
+        laikas: 1583832434388,
+        pranesimas: {
+            tekstas: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex earum tenetur odit nemo id architecto laborum iure error, aliquid nisi . Ex earum tenetur  . Ex earum tenetur odit nemo id architecto laborum iure erro',
+            spalva: '',
+            paveiksliukai: [
+                '1.png',
+            ]
+        }
+    },
+    {
+        autorius: {
+            vardas: 'Vardenis',
+            pavarde: 'Pavardenis',
+            amzius: 50,
+            lytis: 'Vyras',
+            avataras: 'avataras.png',
+            nuoroda: '#',
+        },
+        laikas: 1583832434388,
+        pranesimas: {
+            tekstas: 'LabasLorem ipsum dolor sit, amet consectetur adipisicing elit. Ex earum tenetur odit nemo id architecto laborum iure error, aliquid nisi, Ex earum tenetur odit nemo id architecto laborum iure erro',
+            spalva: '',
+            paveiksliukai: [
+                '1.png',
+                '2.png',
+                '3.png',
+            ]
+        }
+    },
+    {
+        autorius: {
+            vardas: 'Vardenis',
+            pavarde: 'Pavardenis',
+            amzius: 50,
+            lytis: 'Vyras',
+            avataras: 'avataras.png',
+            nuoroda: '#',
+        },
+        laikas: 1583832434388,
+        pranesimas: {
+            tekstas: 'LabasLorem ipsum dolor sit, amet consectetur adipisicing elit. Ex earum ',
+            spalva: 'orange',
+            paveiksliukai: [
+                '1.png',
+                '2.png',
+                '3.png',
+                '4.png',
+            ]
+        }
+    },
+
+    {
+        autorius: {
+            vardas: 'Vardenis',
+            pavarde: 'Pavardenis',
+            amzius: 50,
+            lytis: 'Vyras',
+            avataras: 'avataras.png',
+            nuoroda: '#',
+        },
+        laikas: 1583832434388,
+        pranesimas: {
+            tekstas: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex earum tenetur odit nemo id architecto laborum iure error, aliquid nisi',
+            spalva: '',
+            paveiksliukai: [
+                '1.png',
+                '2.png',
+                '3.png',
+                '4.png',
+                '5.png',
+                '6.png',
+            ]
+        }
+    },
+
+]
+
+// console.log(feed);
---
+++ js/main.js
@@ -0,0 +1,62 @@
+"use strict";
+
+function getData(data) {
+
+    let HTML = '';
+    let sectionBody = document.querySelector('.section');
+    
+    
+    if (Array.isArray(data)) {
+
+        for (let i = 0; i < data.length; i++) {
+            
+            HTML += getList(data[i]);
+        }
+
+        return sectionBody.innerHTML = HTML;
+        // return console.log(HTML);
+    } else {
+        return console.log('Tai ne masyvas!');
+    }
+    
+
+}
+
+function getList(list) {
+
+    console.log(list);
+    let HTML = `  <div class="card">
+                  <div class="card__head">
+                    <img src="./img/avatar/user.png" alt="avatar">
+                 <div class="user">
+                   <div class="name">${list.autorius.vardas} ${list.autorius.pavarde}</div>
+                   <div class="time">${list.laikas}</div>
+                </div>
+                    <div class="more">
+                      <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                       </div>
+                     </div>
+                 <div class="card__main">${list.pranesimas.tekstas}</div>
+                 <div class="card__footer"></div>
+                 </div>`;
+    
+    return HTML;
+}
+
+function createHeader() {
+
+}
+
+function createMain() {
+
+}
+
+function createFooter() {
+
+}
+
+
+
+//!============ ! =============! \\
+getData(feed);
+
