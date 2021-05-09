02cff2a36dbd70c466fe07ea2417600c387cad7d
Myk0l4s <morkunas.mykolas@gmail.com>
2021-05-09 20:22:27 +0300

    fixed whole card

--- js/main.js
+++ js/main.js
@@ -27,20 +27,38 @@ function getData(data) {
 function getList(list) {
     
     console.log(list.autorius.vardas);
-    let HTML = `  <div class="card">
-                  <div class="card__head">
-                    <img src="./img/avatar/user.png" alt="avatar">
-                 <div class="user">
-                   <div class="name">${list.autorius.vardas} ${list.autorius.pavarde}</div>
-                   <div class="time">${list.laikas}</div>
-                </div>
-                    <div class="more">
-                      <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-                       </div>
-                     </div>
-                 <div class="card__main">${list.pranesimas.tekstas}</div>
-                 <div class="card__footer"></div>
-                 </div>`;
+    let HTML = `                   <div class="card">
+    <div class="card__head">
+      <img src="./img/avatar/user.png" alt="avatar">
+    <div class="user">
+        <div class="name">${list.autorius.vardas} ${list.autorius.pavarde}</div>
+        <div class="time">${list.laikas}</div>
+    </div>
+    <div class="more">
+       <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+    </div>
+   </div>
+   <div class="card__main">
+       <p>${list.pranesimas.tekstas}</p>
+       <div class="card__like">
+           <a href="#"><i class="fa fa-thumbs-up" aria-hidden="true">Like</i></a>
+           <a href="#"><i class="fa fa-comment" aria-hidden="true"></i>Comment</a>
+           <a href="#"><i class="fa fa-share" aria-hidden="true"></i>Share</a>
+       </div>
+   </div>
+   <div class="card__footer">
+       <img src="./img/avatar/user.png" alt="avatar">
+       <form method="GET" class="comment">
+           <input type="comment" placeholder="Comments" class="input__comment">
+       </form>
+       <div class="card__icons">
+           <a href="#"><i class="fa fa-smile-o" aria-hidden="true"></i></a>
+           <a href="#"><i class="fa fa-camera" aria-hidden="true"></i></a>
+           <a href="#"><i class="fa fa-picture-o" aria-hidden="true"></i></a>
+           <a href="#"><i class="fa fa-user-circle" aria-hidden="true"></i></a>
+       </div>
+   </div>
+</div>`;
     
     return HTML;
 }
@@ -62,3 +80,17 @@ function createFooter() {
 //!============ ! =============! \\
 getData(feed);
 
+{/* <div class="card">
+                  <div class="card__head">
+                    <img src="./img/avatar/user.png" alt="avatar">
+                 <div class="user">
+                   <div class="name">${list.autorius.vardas} ${list.autorius.pavarde}</div>
+                   <div class="time">${list.laikas}</div>
+                </div>
+                    <div class="more">
+                      <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                       </div>
+                     </div>
+                 <div class="card__main">${list.pranesimas.tekstas}</div>
+                 <div class="card__footer"></div>
+                 </div> */}
