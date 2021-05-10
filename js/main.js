ae8e1f2f56269950496532ae939477192194a80d
Myk0l4s <morkunas.mykolas@gmail.com>
2021-05-10 17:51:36 +0300

    card header/main/footer split on js into different functions

--- js/main.js
+++ js/main.js
@@ -27,41 +27,64 @@ function getData(data) {
 function getList(list) {
     
     // console.log(list.autorius.vardas);
-    let HTML = ` <div class="card">
-    <div class="card__head">
-      <img src="./img/avatar/user.png" alt="avatar">
-    <div class="user">
-        <div class="name">${list.autorius.vardas} ${list.autorius.pavarde}</div>
-        <div class="time">${list.laikas}</div>
-    </div>
-    <div class="more">
-       <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-    </div>
-   </div>
-   <div class="card__main">
-       <p>${list.pranesimas.tekstas}</p>
-       <div class="card__like">
-           <a href="#"><i class="fa fa-thumbs-up" aria-hidden="true">Like</i></a>
-           <a href="#"><i class="fa fa-comment" aria-hidden="true"></i>Comment</a>
-           <a href="#"><i class="fa fa-share" aria-hidden="true"></i>Share</a>
-       </div>
-   </div>
-   <div class="card__footer">
-       <img src="./img/avatar/user.png" alt="avatar">
-       <form method="GET" class="comment">
-           <input type="comment" placeholder="Comments" class="input__comment">
-       </form>
-       <div class="card__icons">
-           <a href="#"><i class="fa fa-smile-o" aria-hidden="true"></i></a>
-           <a href="#"><i class="fa fa-camera" aria-hidden="true"></i></a>
-           <a href="#"><i class="fa fa-picture-o" aria-hidden="true"></i></a>
-           <a href="#"><i class="fa fa-user-circle" aria-hidden="true"></i></a>
-       </div>
-   </div>
-</div>`;
+    let HTML = ` ${cardHeader(list)}
+                 ${cardMain(list)}
+                 ${cardFooter(list)}`;
     
     return HTML;
 }
+
+//!!!      Funkcijos atskiriems card part'ams ===========
+
+function cardHeader(list) {
+    let HTML = `<div class="card">
+    <div class="card__head">
+    <img src="./img/avatar/user.png" alt="avatar">
+       <div class="user">
+         <div class="name">${list.autorius.vardas} ${list.autorius.pavarde}</div>
+          <div class="time">${list.laikas}</div>
+       </div>
+  <div class="more">
+     <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+  </div>
+  </div>`
+    
+    return HTML;
+}
+
+function cardMain(list) {
+    let HTML = `<div class="card__main">
+        <p>${list.pranesimas.tekstas}</p>
+    <div class="card__like">
+        <a href="#"><i class="fa fa-thumbs-up" aria-hidden="true">Like</i></a>
+        <a href="#"><i class="fa fa-comment" aria-hidden="true"></i>Comment</a>
+        <a href="#"><i class="fa fa-share" aria-hidden="true"></i>Share</a>
+    </div>
+</div>`
+    
+    return HTML;
+}
+
+function cardFooter(list) {
+    let HTML = `<div class="card__footer">
+    <img src="./img/avatar/user.png" alt="avatar">
+    <form method="GET" class="comment">
+        <input type="comment" placeholder="Comments" class="input__comment">
+    </form>
+    <div class="card__icons">
+        <a href="#"><i class="fa fa-smile-o" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-camera" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-picture-o" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-user-circle" aria-hidden="true"></i></a>
+    </div>
+</div>
+</div>`
+    
+    return HTML;
+}
+
+//!!!      Funkcijos atskiriems card part'ams ==================
+
 
 // function timeElapsed(list) {
 //     // console.log(list);
@@ -87,70 +110,10 @@ function getList(list) {
     
 //     return time;
 // }
-//!!!      Funkcijos atskiriems card part'ams ===========
 
-// function cardHeader(list) {
-//     let head = `<div class="card">
-//     <div class="card__head">
-//     <img src="./img/avatar/user.png" alt="avatar">
-//        <div class="user">
-//          <div class="name">${list.autorius.vardas} ${list.autorius.pavarde}</div>
-//           <div class="time">${list.laikas}</div>
-//        </div>
-//   <div class="more">
-//      <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-//   </div>
-//   </div>`
-    
-//     return head;
-// }
-
-// function cardMain(list) {
-//     let main = `<div class="card__main">
-//         <p>${list.pranesimas.tekstas}</p>
-//     <div class="card__like">
-//         <a href="#"><i class="fa fa-thumbs-up" aria-hidden="true">Like</i></a>
-//         <a href="#"><i class="fa fa-comment" aria-hidden="true"></i>Comment</a>
-//         <a href="#"><i class="fa fa-share" aria-hidden="true"></i>Share</a>
-//     </div>
-// </div>`
-    
-//     return main;
-// }
-
-// function cardFooter(list) {
-//     let footer = `<div class="card__footer">
-//     <img src="./img/avatar/user.png" alt="avatar">
-//     <form method="GET" class="comment">
-//         <input type="comment" placeholder="Comments" class="input__comment">
-//     </form>
-//     <div class="card__icons">
-//         <a href="#"><i class="fa fa-smile-o" aria-hidden="true"></i></a>
-//         <a href="#"><i class="fa fa-camera" aria-hidden="true"></i></a>
-//         <a href="#"><i class="fa fa-picture-o" aria-hidden="true"></i></a>
-//         <a href="#"><i class="fa fa-user-circle" aria-hidden="true"></i></a>
-//     </div>
-// </div>
-// </div>`
-    
-//     return footer;
-// }
-
-//!!!      Funkcijos atskiriems card part'ams ==================
 
 //!============ ! =============! \\
+
 getData(feed);
 
 
-
-
-
-
-//! ====== section functions put together
-
-
-// ` <section class="section">
-//                     ${cardHeader(list)}
-//                     ${cardMain(list)}
-//                     ${cardFooter(list)}
-//                     </section>`
